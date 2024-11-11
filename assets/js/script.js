864dd2cbe07dcdab4914c54498c8d0a7c8297b5f
immark007 <immark5507@gmail.com>
2024-11-11 06:09:03 -0300

    estilizando a main, criando componente de button e deixando os responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,51 @@ function menuShow() {
         menuIcon.classList.add('fa-bars');
     }
 }
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active", i === index);
+    });
+    dots.forEach((dot, i) => {
+        dot.classList.toggle("active", i === index);
+    });
+}
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function previousSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+function goToSlide(index) {
+    currentSlide = index;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+let startX = 0;
+let endX = 0;
+
+document.getElementById("slides-container").addEventListener("touchstart", (event) => {
+    startX = event.touches[0].clientX;
+});
+
+document.getElementById("slides-container").addEventListener("touchmove", (event) => {
+    endX = event.touches[0].clientX;
+});
+
+document.getElementById("slides-container").addEventListener("touchend", () => {
+    if (startX > endX + 50) {
+        nextSlide();
+    } else if (startX < endX - 50) {
+        previousSlide();
+    }
+});

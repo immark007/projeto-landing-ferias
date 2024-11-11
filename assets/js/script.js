1877b719dd7d5ef30481d917e5bc73dc0427b4ee
immark007 <immark5507@gmail.com>
2024-11-11 15:39:40 -0300

    atualizando menu, e concluindo animação de carrosel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 function menuShow() {
     const menuMobile = document.querySelector('.mobile-menu');
     const menuIcon = document.getElementById('menu-icon');
-    
+
     menuMobile.classList.toggle('open');
-    
+
     if (menuMobile.classList.contains('open')) {
-        menuIcon.classList.remove('fa-bars');
-        menuIcon.classList.add('fa-xmark');
+        menuIcon.classList.replace('fa-bars', 'fa-xmark');
     } else {
-        menuIcon.classList.remove('fa-xmark');
-        menuIcon.classList.add('fa-bars');
+        menuIcon.classList.replace('fa-xmark', 'fa-bars');
     }
 }
+
 
 let currentSlide = 0;
 const slides = document.querySelectorAll(".slide");
